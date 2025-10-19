3d2880875d85c89ba1c5fc8b66cc82278927fe8c
Yanchik07 <yanchik.valeev07@yandex.ru>
2025-10-19 23:02:39 +0400

    improve yandex metrika tracking for SPA

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Инициализация Яндекс.Метрики для SPA
+if (typeof window !== 'undefined' && window.ym) {
+  // Отслеживаем загрузку страницы
+  window.ym(104708408, 'hit', window.location.href);
+}
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
